10657089847375d97c7493d382f37e2b7d0160c1
Jonas Franke <jonasfranke@sdevs.org>
2024-09-08 20:52:52 +0200

    Refactor getLatestVersionForRange function to handle errors and return error messages

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,42 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import semver from "semver";
 
 export const getLatestVersion = async (packageName: string) => {
-    const { data } = await axios.get(`https://registry.npmjs.org/${packageName}/latest`);
-    return data.version;
+    const res = (await axios.get(`https://registry.npmjs.org/${packageName}/latest`).catch(() => undefined)) as
+        | AxiosResponse<any, any>
+        | undefined;
+
+    const errors: string[] = [];
+
+    if (!res || res.status !== 200) {
+        errors.push(`\nPackage ${packageName} not found`);
+        return { latestVersion: null, errors };
+    }
+
+    const data = res.data;
+    return { latestVersion: data.version, errors };
 };
 
 export const getLatestVersionForRange = async (packageName: string, range: string) => {
-    const { data } = await axios.get(`https://registry.npmjs.org/${packageName}`);
+    const res = (await axios.get(`https://registry.npmjs.org/${packageName}`).catch(() => undefined)) as
+        | AxiosResponse<any, any>
+        | undefined;
+
+    let errors: string[] = [];
+
+    if (!res || res.status !== 200) {
+        errors.push(`\nPackage ${packageName} not found`);
+        return { latestVersion: null, errors };
+    }
+
+    const data = res.data;
+
     const versions = Object.keys(data.versions);
     const validVersions = versions.filter((version) => semver.satisfies(version, range));
-    const latestVersion = validVersions.sort((a, b) => semver.rcompare(a, b))[0];
-    return latestVersion;
+    const latestVersion = validVersions
+        .sort((a, b) => semver.rcompare(a, b))[0]
+        .replace(/\^|~/, "")
+        .replace("=", "");
+
+    return { latestVersion, errors };
 };
